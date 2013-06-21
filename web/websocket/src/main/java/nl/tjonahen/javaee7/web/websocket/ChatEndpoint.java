9b799c09d2672efbc31472fe181c4f394ab6df27
Philippe Tjon-A-Hen <philippe@tjonahen.nl>
2013-06-21 14:04:09 +0200

    fixed poms afther failed release

---
+++ web/websocket/src/main/java/nl/tjonahen/javaee7/web/websocket/ChatEndpoint.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2013 Philippe Tjon-A-Hen philippe@tjonahen.nl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.tjonahen.javaee7.web.websocket;
+
+import java.io.IOException;
+import javax.websocket.OnMessage;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ *
+ * @author Philippe Tjon-A-Hen philippe@tjonahen.nl
+ */
+@ServerEndpoint("/chat")
+public class ChatEndpoint {
+
+    @OnMessage
+    public void onMessage(final String message, final Session session) throws IOException {
+        for (final Session clientSession: session.getOpenSessions()) {
+            if (clientSession.isOpen()) {
+                clientSession.getBasicRemote().sendText(message);
+            }
+        }        
+    }
+    
+}
---
+++ web/websocket/src/main/java/nl/tjonahen/javaee7/web/websocket/Delayed.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2013 Philippe Tjon-A-Hen philippe@tjonahen.nl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.tjonahen.javaee7.web.websocket;
+
+import javax.websocket.OnMessage;
+import javax.websocket.server.ServerEndpoint;
+
+/**
+ *
+ * @author Philippe Tjon-A-Hen philippe@tjonahen.nl
+ */
+@ServerEndpoint("/delayed")
+public class Delayed {
+
+    @OnMessage
+    public String onMessage(String message) throws InterruptedException {
+        Thread.sleep(10000);
+        return message;
+    }
+    
+}
